b7f10df71c75a20015437981025b950d869b7db1
ttshimiz <tshimizu@astro.umd.edu>
2016-05-27 00:19:55 -0400

    create plots of results for sersic+psf fits

--- plot_galfit_results.py
+++ plot_galfit_results.py
@@ -14,17 +14,14 @@ bat_info = bat_info.drop(np.array(['Mrk3', 'NGC3079']))
 pacs_info = pd.read_csv(morph_dir+'pacs70_image_info.csv', index_col=0)
 
 names = bat_info.index.values
-#names = ['PG2304+042', 'NGC7582', 'NGC7469', 'NGC6860',
-#         'NGC6240', 'NGC5728', 'NGC5506', 'NGC4151', 'NGC4051',
-#         'NGC3783', 'NGC3281', 'NGC3081', 'NGC2992', 'NGC1365', 'Mrk841',
-#         'Mrk348', 'Mrk335', 'Mrk290', 'IISZ010', 'IC5063', 'CenA']
-out_dir = morph_dir+'results/sersic/PACS70/'
+#names = ['NGC2992']
+out_dir = morph_dir+'results/sersicANDpsf/PACS70/'
 
 for n in names:
     
-    out_file = out_dir+n+'_pacs70_sersic.fits'
+    out_file = out_dir+n+'_pacs70_sersicANDpsf.fits'
     pra = np.int(pacs_info.loc[n, 'PSF Rotation Angle'])
-    if (os.path.exists(out_file)) & (pra != 180):
+    if (os.path.exists(out_file)):
         fig = bmt.plot_galfit_results(out_file, n)
-        fig.savefig(out_dir+n+'_pacs70_sersic.pdf', bbox_inches='tight')
+        fig.savefig(out_dir+n+'_pacs70_sersicANDpsf.pdf', bbox_inches='tight')
         plt.close(fig)
